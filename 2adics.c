5150080215a423501ebb34d983ac6e93ee0f33f9
Michael <135757624+StudioGuma@users.noreply.github.com>
2023-10-29 14:16:04 -0400

    Add files via upload

---
+++ 2adics.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <math.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return abs(a);
+}
+
+int twoadic(int a, int b) {
+    long long x = 4611686018427387903; // Last 32 bits are 1’s
+    while (x % b != 0 && x > 4294967295) {
+        x++;
+        x >>= 1;
+        x--;
+    }
+    long long y = -x/b;
+    return (int) y*a;
+}
+
+void printbin(int a) {
+    for (int i = sizeof(a)*8 - 1; i >= 0; i--) putchar(((a >> i) & 1) + 48); // 0 is ASCII character 48
+}
+
+int valuation(int a) { // Exponent of the highest power of 2 that divides a
+    int val = 0;
+    while (fmod(a, exp2(val + 1)) == 0) val++;
+    return val;
+}
+
+double twoabs(int a, int b) {
+    if (a == 0) return 0;
+    return 1/exp2(valuation(a) - valuation(b));
+}
+
+int main() {
+    int a, b;
+    char c;
+
+    do {
+        printf("\nEnter the numerator: ");
+        scanf("%d", &a);
+        printf("Enter the denominator (1 for integer): ");
+        scanf("%d", &b);
+
+        if (b == 0) printf("Can't divide by 0");
+        else {
+            if (b < 0) {
+                a *= -1;
+                b *= -1;
+            }
+            int d = gcd(a, b);
+            a /= d;
+            b /= d; // Reduce a/b to canonical form
+
+            if (b % 2 == 0) printf("%d/%d doesn't have a 2-adic representation", a, b);
+            else {
+                if (b == 1) printf("2-adic representation of %d: ...", a);
+                else printf("2-adic representation of %d/%d: ...", a, b);
+                printbin(twoadic(a, b));
+            }
+            
+            if (b == 1) printf("\n2-adic absolute value of %d (in decimal): %g", a, twoabs(a, b));
+            else printf("\n2-adic absolute value of %d/%d (in decimal): %g", a, b, twoabs(a, b));
+        }
+        printf("\nContinue? (y/n) ");
+        scanf("%s", &c);
+    } while (c == 'y' || c == 'Y');
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Command line program that calculates & prints the last 32 digits of the representation of a rational number as a 2-adic integer, as well as its 2-adic absolute value. Takes 2 integer inputs, the numerator a & the denominator b, to make the rational number a/b.
+
+[Read here](https://en.wikipedia.org/wiki/P-adic_number) if you don’t know what p-adics are, but basically, it’s a way of writing a rational number in base p with infinitely many digits, which works best if p is a prime number. For every p^n, the higher n is, the closer to 0 the absolute value is.
+
+Works for any a & b between -2147483648 & 2147483647 inclusive, but a/b will be reduced to canonical form (a & b are coprime; b > 0). Every a/b has a 2-adic expansion so long as b isn’t divisible by 2, tested to be accurate for smaller denominators. For integers (i.e. for b = 1), it’s conveniently the same as two’s complement.
+
+Building requires installing the GCC compiler and, on Windows, Make. To build, open a command line & type `make`. You can then execute by typing `2adics` (on Windows) or `./2adics` (on Unix-like systems), and clean by typing `make clean`.  
+**NOTE:** If you’re not using Windows, you need to change the `OS` value in the makefile out of `windows`, or cleaning won’t work.
+
+File size: 397 KB
